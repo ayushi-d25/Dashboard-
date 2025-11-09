b289fd5d012f8289e114d1cd7a1edfb4f287de32
Ayushi_Dutta <ayushi.dutta25@gmail.com>
2025-11-09 23:54:01 +0530

    second commit

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,25 @@ const taskRouter = require("./src/controller/taskController");
 const { successResponse } = require("./src/middleware/responseStructure");
 const { errorHandler } = require("./src/middleware/errorHandler");
 const connectDB = require("./src/utils/db");
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 const port = parseInt(process.env.port, 10) || 3000;
 
-app.use(cors());
+
+app.use(cors({
+  origin: 'https://profound-sawine-1fef06.netlify.app', // your Netlify frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // if using cookies or auth headers
+}));
+
+
+app.options('*', cors({
+  origin: 'https://profound-sawine-1fef06.netlify.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -22,5 +35,5 @@ app.use(errorHandler);
 
 app.listen(port, () => {
   connectDB();
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server listening on port ${port} `);
 });
